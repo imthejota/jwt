6b5ee105e5a409d47f5b07cf8224dbf14367cc3c
Jonathan Kechian <jkechian@gmail.com>
2023-06-01 00:51:22 -0300

    error handling http status codes + index

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,22 @@
 const jwt = require('jsonwebtoken')
-const customApiError = require('../errors/custom-error')
+const {UnauthenticatedError} = require('../errors')
 
 
 
 const authenticationMiddleware = async (req, res, next) => {
     const authHeader = req.headers.authorization
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {throw new customApiError('No token provided', 401)}
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {throw new UnauthenticatedError('No token provided')}
     
     const token = authHeader.split(' ')[1]
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        console.log(decoded)
-        const luckyNumber = Math.floor(Math.random()*100)
-        res.status(200).json({msg: `Hello ${decoded.username}`, secret: `Your token is: ${luckyNumber}`})
+        const {id, username} = decoded
+        req.user = {id, username}
+        next()
     } catch (error) {
-        throw new customApiError('Route not valid for you. Get back!', 401)
+        throw new UnauthenticatedError('Route not valid for you. Get back!')
     }
     console.log(req.headers.authorization);
-    next()
 }
 
 module.exports = authenticationMiddleware
